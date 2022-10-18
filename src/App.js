ae4743faa619405fc353779236a6ffb10743b1b1
RayhanalKavey <rayhanalkavey@gmail.com>
2022-10-19 03:53:52 +0600

    Create Layout, Folder Structure and Basic Components.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
-
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes/Routes";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
 }
 
 export default App;
