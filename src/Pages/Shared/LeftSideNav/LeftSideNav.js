9bc82b7c681099a592d8f6c82cfa5d71c7254260
RayhanalKavey <rayhanalkavey@gmail.com>
2022-10-19 05:10:39 +0600

    Create Side Components with data from server.

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
   const [categories, setCategories] = useState([]);
   useEffect(() => {
-    fetch("");
+    fetch("http://localhost:5005/news-categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
   }, []);
+  // console.log(categories);
   return (
     <div>
       <h4>All category</h4>
+      {categories.map((category) => (
+        <p key={category.id}>
+          <Link to={`category/${category.id}`}>{category.name}</Link>
+        </p>
+      ))}
     </div>
   );
 };
