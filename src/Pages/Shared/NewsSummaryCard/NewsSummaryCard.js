227d129caa66626e962f31127992e2d1530b38ac
RayhanalKavey <rayhanalkavey@gmail.com>
2022-10-20 20:17:57 +0600

    Add style to the NewsSummary component.

--- src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
+++ src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
@@ -1,7 +1,47 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import Image from "react-bootstrap/Image";
+import { FaRegBookmark, FaShareAlt, IconName } from "react-icons/fa";
 
 const NewsSummaryCard = ({ news }) => {
-  return <div>{news.title}</div>;
+  const { _id, title, total_view, author, details, image_url } = news;
+  const { name, published_date, img } = author;
+  console.log(news);
+  return (
+    <Card className=" mb-5">
+      <Card.Header>
+        <div className="d-flex justify-content-between align-items-center">
+          <div className="d-flex gap-2 justify-content-center align-items-start">
+            <Image roundedCircle style={{ height: "60px" }} src={img}></Image>
+            <div>
+              <h5>{name}</h5>
+              <p>{published_date}</p>
+            </div>
+          </div>
+          <div>
+            <FaRegBookmark></FaRegBookmark>
+            <FaShareAlt></FaShareAlt>
+          </div>
+        </div>
+      </Card.Header>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Img variant="top" src={image_url} />
+        <Card.Text>
+          {details.length > 250 ? (
+            <p>
+              {details.slice(0, 250) + "...."}{" "}
+              <Link to={`news/${_id}`}>Read more</Link>
+            </p>
+          ) : (
+            <p>{details}</p>
+          )}{" "}
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+    </Card>
+  );
 };
 
 export default NewsSummaryCard;
